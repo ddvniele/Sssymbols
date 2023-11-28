cabc20f17ff2b1fb6d264fdcb71de6da311e3e95
ddvniele <ddvniele@gmail.com>
2023-11-28 14:32:45 +0100

    Initial Commit

---
+++ Sssymbols/SssymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SssymbolsApp.swift
+//  Sssymbols
+//
+//  Created by dan on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SssymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
